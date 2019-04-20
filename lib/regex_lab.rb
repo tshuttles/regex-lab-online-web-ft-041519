82b6c9c31d06791faf8f50438a8ef27fecfe2429
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-20 00:12:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[0-9]{3}/) ? true : false 
+  phone.scan(/^[0-9]{3}[0-9]{3}[0-9]{4}$/) ? true : false 
 end
